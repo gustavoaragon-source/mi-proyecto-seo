78391b3e1d5292b2a78c2f1144253504f85610e6
gustavoaragon <gustavo.aragon@tecsup.edu.pe>
2026-06-18 11:01:55 -0500

    Simular CMS para generacion del sitemap

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,7 +1,8 @@
 const BASE_URL = "https://mi-proyecto-seo-seven.vercel.app";
 
-export default async function handler(req, res) {
-  const pages = [
+// Simulación de consulta a una base de datos o CMS
+async function getPagesFromCMS() {
+  return [
     {
       slug: "",
       updatedAt: new Date().toISOString(),
@@ -15,6 +16,10 @@ export default async function handler(req, res) {
       updatedAt: new Date().toISOString(),
     },
   ];
+}
+
+export default async function handler(req, res) {
+  const pages = await getPagesFromCMS();
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -24,14 +29,12 @@ ${pages
   <url>
     <loc>${BASE_URL}/${page.slug}</loc>
     <lastmod>${page.updatedAt}</lastmod>
-  </url>
-`
+  </url>`
   )
   .join("")}
 </urlset>`;
 
   res.setHeader("Content-Type", "text/xml");
-
   res.write(sitemap);
   res.end();
 }
